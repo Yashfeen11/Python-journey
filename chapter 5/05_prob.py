8d39df818a520d0228cf3e235b6aa3a03737ce5d
Yashfeen11 <yashfeen6343@gmail.com>
2025-08-10 15:30:13 +0530

    Create 05_prob.py

---
+++ chapter 5/05_prob.py
@@ -0,0 +1,17 @@
+a={}
+name = input("Enter friends name:")
+lang = input("Enter language name:")
+a.update({name: lang})
+
+name = input("Enter friends name:")
+lang = input("Enter language name:")
+a.update({name: lang})
+
+name = input("Enter friends name:")
+lang = input("Enter language name:")
+a.update({name: lang})
+
+name = input("Enter friends name:")
+lang = input("Enter language name:")
+a.update({name: lang})
+print(a)
