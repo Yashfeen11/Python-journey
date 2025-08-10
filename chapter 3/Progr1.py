26bc4475459050124794e116a6f85be7ee95be2a
Yashfeen11 <yashfeen6343@gmail.com>
2025-08-10 08:10:57 +0530

    fstring
    another way of using variable directly in the print line

---
+++ chapter 3/Progr1.py
@@ -0,0 +1,6 @@
+"""to display a user entered name followed by Good 
+Afternoon using input () function. """
+a=input("Enter the name:")
+# print(a,"Good Afternoon")
+print(f"Good Afternoon, {a}") 
+#f string we can directly add the variable using this
