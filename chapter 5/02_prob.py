17ff55601a0d1d8e387eb48b24d8f53327debfe1
Yashfeen11 <yashfeen6343@gmail.com>
2025-08-10 15:30:05 +0530

    Create 02_prob.py

---
+++ chapter 5/02_prob.py
@@ -0,0 +1,11 @@
+a1=int(input("Enter the number 1:"))
+a2=int(input("Enter the number 2:"))
+a3=int(input("Enter the number 3:"))
+a4=int(input("Enter the number 4:"))
+a5=int(input("Enter the number 5:"))
+a6=int(input("Enter the number 6:"))
+a7=int(input("Enter the number 7:"))
+a8=int(input("Enter the number 8:"))
+set={a1,a2,a3,a4,a5,a6,a7,a8}
+print(set)
+
