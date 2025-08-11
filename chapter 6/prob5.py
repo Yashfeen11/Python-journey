ba003e552a3b56a08401758317de85603b6f3ef4
Yashfeen11 <yashfeen6343@gmail.com>
2025-08-11 08:41:35 +0530

    Create prob5.py

---
+++ chapter 6/prob5.py
@@ -0,0 +1,17 @@
+my_list=[]
+name1=input("Enter the name:")
+my_list.append(name1)
+name2=input("Enter the name:")
+my_list.append(name2)
+name3=input("Enter the name:")
+my_list.append(name3)
+name4=input("Enter the name:")
+my_list.append(name4)
+name5=input("Enter the name:")
+my_list.append(name5)
+print(my_list)
+if("Harry" in my_list):
+    print("Name found")
+
+else:
+    print("Name not found")
