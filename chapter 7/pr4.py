5a6c7a39cb1d01b874c0fb78351a6951c646a780
Yashfeen11 <yashfeen6343@gmail.com>
2025-09-03 14:41:55 +0530

    Create pr4.py

---
+++ chapter 7/pr4.py
@@ -0,0 +1,10 @@
+num=int(input("Enter the number: "))
+c=0
+for i in range(1,num+1):
+    if(num%i==0):
+        c+=1
+
+if(c==2):
+    print(f"{num} is prime")
+else:
+    print(f"{num} is not prime")
